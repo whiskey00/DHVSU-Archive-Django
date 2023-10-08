26f3a9182192976168ed1f15b4ca02a319821115
rZulan <roi.zulan@gmail.com>
2023-10-09 00:33:59 +0800

    tangol elol

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,12 +3,26 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+    error_messages = {
+        'password_mismatch': (""),
+        'password_too_short': (""),
+        'password_common': (""),
+        'username': {
+            'required': (""),
+            'unique': (""),
+            'invalid': (""),
+        },
+        'email': {
+            'required': (""),
+            'unique': (""),
+            'invalid': (""),
+        },
+    }
 
-
-    username = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Username"}))
-    email = forms.EmailField(label="", required=True, widget=forms.EmailInput(attrs={"placeholder": "myemail@dhvsu.edu.ph"}))
-    password1 = forms.CharField(label="", widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
-    password2 = forms.CharField(label="", widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
+    username = forms.CharField(label="", error_messages=error_messages, widget=forms.TextInput(attrs={"placeholder": "Username"}))
+    email = forms.EmailField(label="", error_messages=error_messages, required=True, widget=forms.EmailInput(attrs={"placeholder": "myemail@dhvsu.edu.ph"}))
+    password1 = forms.CharField(label="", error_messages=error_messages, widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
+    password2 = forms.CharField(label="", error_messages=error_messages, widget=forms.PasswordInput(attrs={"placeholder": "Confirm Password"}))
 
     class Meta:
         model = User
