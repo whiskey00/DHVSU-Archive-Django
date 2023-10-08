9193f412d68f668ea49013450df06beefb6f639c
rZulan <roi.zulan@gmail.com>
2023-10-09 00:16:33 +0800

    comint

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,6 +3,8 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class RegisterForm(UserCreationForm):
+
+
     username = forms.CharField(label="", widget=forms.TextInput(attrs={"placeholder": "Username"}))
     email = forms.EmailField(label="", required=True, widget=forms.EmailInput(attrs={"placeholder": "myemail@dhvsu.edu.ph"}))
     password1 = forms.CharField(label="", widget=forms.PasswordInput(attrs={"placeholder": "Password"}))
@@ -20,7 +22,7 @@ class RegisterForm(UserCreationForm):
     def clean_email(self, *args, **kwargs):
         email = self.cleaned_data.get("email")
 
-        if not "dhvsu.edu.ph" in email:
-            raise forms.ValidationError("Must be a DHVSU account!")
+        # if not "dhvsu.edu.ph" in email:
+        #     raise forms.ValidationError("Must be a DHVSU account!")
         
         return email
